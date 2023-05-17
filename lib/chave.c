867877a2def49ed7b3bb3bdbcb9d2aa7937bbcc1
Gabriel Felipe Majeau Lopes <PUCMINAS\1269088@PPC01INFLL00621.pucminas.net>
2023-05-17 20:40:51 -0300

    upload folders

---
+++ lib/chave.c
@@ -0,0 +1,19 @@
+#include "../include/chave.h"
+#include "../include/dados.h"
+#include "../include/terminal.h"
+
+int setChave(void) {
+
+
+    /*
+    *               PAGINA DE CADASTRO
+    *
+    *  Input dos Nomes dos Times
+    * 
+    *  Inserção dos Times na Estrutura de Dados e na Memória
+    * 
+    *  Chama e Recebe o return das Funções de Simulação
+    */
+
+
+}
---
+++ src/index.c
@@ -0,0 +1,18 @@
+#include "../include/terminal.h"
+#include "../include/chave.h"
+#include "../include/tabela.h"
+
+int main(void){
+
+
+    /*
+    *                   MENU INICIAL
+    *
+    *  Input do Nome do Campeonato e da Quantidade de Times
+    * 
+    *  Chama e Recebe o return das Funções setChave() ou setTabela()
+    */
+
+
+    return 1;
+}
